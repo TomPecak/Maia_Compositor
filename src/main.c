0f4a50ba41f0f812188b0e8702226bb622003e4b
Tom Pecak <tom85.pecak@gmail.com>
2026-06-16 11:32:24 +0200

    rapair CMakeLists.txt, wyland protocols works fine

--- src/main.c
+++ src/main.c
@@ -1,20 +1,17 @@
 
-#include <stdlib.h>
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <wayland-server-core.h>
+#include <wlr/backend.h>
 #include <wlr/render/allocator.h>
 #include <wlr/render/wlr_renderer.h>
-#include <wlr/types/wlr_cursor.h>
 #include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_cursor.h>
 #include <wlr/types/wlr_data_device.h>
-
 #include <wlr/types/wlr_scene.h>
-
 #include <wlr/types/wlr_subcompositor.h>
-
-#include <wlr/backend.h>
+#include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/util/log.h>
 
 struct tinywl_server {
@@ -24,6 +21,11 @@ struct tinywl_server {
   struct wlr_allocator* allocator;
   struct wlr_scene *scene;
   struct wlr_scene_output_layout *scene_layout;
+
+  struct wlr_xdg_shell* xdg_shell;
+  struct wl_listener new_xdg_toplevel;
+  struct wl_listener new_xdg_popup;
+  struct wl_list toplevels;
 
   struct wlr_output_layout *output_layout;
   struct wl_list outputs;
@@ -38,6 +40,13 @@ struct tinywl_output {
   struct wl_listener request_state;
   struct wl_listener destroy;
 };
+
+#define COLOR_RESET "\033[0m"
+#define COLOR_GREEN "\033[32m"
+
+#define maia_info(fmt, ...)                                        \
+  fprintf(stderr, COLOR_GREEN "[MAIA INFO] " COLOR_RESET fmt "\n", \
+          ##__VA_ARGS__)
 
 static void output_frame(struct wl_listener *listener, void *data) {
   /* This function is called every time an output is ready to display a frame,
@@ -138,110 +147,128 @@ static void server_new_output(struct wl_listener *listener, void *data) {
   wlr_scene_output_layout_add_output(server->scene_layout, l_output, scene_output);
 }
 
-int main(int argc, char *argv[]) {
-  wlr_log_init(WLR_DEBUG, NULL);
-  char *startup_cmd = NULL;
+static void server_new_xdg_toplevel(struct wl_listener* listener, void* data) {}
 
-  int c;
-  while ((c = getopt(argc, argv, "s:h")) != -1) {
-    switch (c) {
-      case 's':
-        startup_cmd = optarg;
-        break;
-      default:
-        printf("Usage: %s [-s startup command]\n", argv[0]);
-        break;
-    }
+static void server_new_xdg_popup(struct wl_listener* listener, void* data) {
   }
-  if (optind < argc) {
-    printf("Usage: %s [-s startup command]\n", argv[0]);
+
+  int main(int argc, char* argv[]) {
+    wlr_log_init(WLR_ERROR, NULL);
+    char* startup_cmd = NULL;
+
+    int c;
+    while ((c = getopt(argc, argv, "s:h")) != -1) {
+      switch (c) {
+        case 's':
+          startup_cmd = optarg;
+          break;
+        default:
+          printf("Usage: %s [-s startup command]\n", argv[0]);
+          break;
+      }
+    }
+    if (optind < argc) {
+      printf("Usage: %s [-s startup command]\n", argv[0]);
+      return 0;
+    }
+
+    struct tinywl_server server = {0};
+
+    server.wl_display = wl_display_create();
+    if (server.wl_display == NULL) {
+      wlr_log(WLR_ERROR, "Failed to create wayland display");
+      return 1;
+    }
+
+    server.backend = wlr_backend_autocreate(
+        wl_display_get_event_loop(server.wl_display), NULL);
+    if (server.backend == NULL) {
+      wlr_log(WLR_ERROR, "failed to create wlr_backend");
+      return 1;
+    }
+
+    server.renderer = wlr_renderer_autocreate(server.backend);
+    if (server.renderer == NULL) {
+      wlr_log(WLR_ERROR, "failed to create wlr_renderer");
+      return 1;
+    }
+
+    wlr_renderer_init_wl_display(server.renderer, server.wl_display);
+
+    // CREATE GBM BUFFERS
+    server.allocator =
+        wlr_allocator_autocreate(server.backend, server.renderer);
+    if (server.allocator == NULL) {
+      wlr_log(WLR_ERROR, "failed to create wlr_allocator");
+      return 1;
+    }
+
+    // INITIALIZE WAYLAND CORE GLOBALS
+
+    // CORE COMPOSITOR: HANDLES SURFACES AND BINDS GRAPHICS RENDERER
+    wlr_compositor_create(server.wl_display, 5, server.renderer);
+
+    // SUBCOMPOSITOR: ALLOWS NESTED SURFACES (E.G., BROWSER TABS/VIDEO PLAYERS)
+    wlr_subcompositor_create(server.wl_display);
+
+    // DATA DEVICE MANAGER: ENABLES CLIPBOARD AND DRAG-AND-DROP
+    wlr_data_device_manager_create(server.wl_display);
+
+    //?????????????????????????????????????????????????????????????????
+    // server.output_layout = wlr_output_layout_create(server.wl_display);
+    server.output_layout = wlr_output_layout_create(server.wl_display);
+
+    wl_list_init(&server.outputs);
+
+    // SET CALLBACK FOR NEW MONITOR
+    server.new_output.notify = server_new_output;
+    wl_signal_add(&server.backend->events.new_output, &server.new_output);
+
+    server.scene = wlr_scene_create();
+    server.scene_layout =
+        wlr_scene_attach_output_layout(server.scene, server.output_layout);
+
+    wl_list_init(&server.toplevels);
+    server.xdg_shell = wlr_xdg_shell_create(server.wl_display, 3);
+    server.new_xdg_toplevel.notify = server_new_xdg_toplevel;
+    wl_signal_add(&server.xdg_shell->events.new_toplevel,
+                  &server.new_xdg_toplevel);
+    server.new_xdg_popup.notify = server_new_xdg_popup;
+    wl_signal_add(&server.xdg_shell->events.new_popup, &server.new_xdg_popup);
+
+    //------------------ RUN -----------------------
+
+    // Add a Unix socket to the Wayland display
+    const char* socket = wl_display_add_socket_auto(server.wl_display);
+    if (!socket) {
+      wlr_backend_destroy(server.backend);
+      return 1;
+    }
+
+    // START THE BACKEND. THIS WILL ENUMERATE OUTPUTS AND INPUTS, BECOME THE DRM
+    // MASTER, etc
+    if (!wlr_backend_start(server.backend)) {
+      wlr_backend_destroy(server.backend);
+      wl_display_destroy(server.wl_display);
+    }
+
+    // SET ENVIROMENT VARIABLE
+    setenv("WAYLAND_DISPLAY", socket, true);
+    if (startup_cmd) {
+      if (fork() == 0) {
+        execl("/bin/sh", "/bin/sh", "-c", startup_cmd, (void*)NULL);
+      }
+    }
+
+    wlr_log(WLR_INFO, "Running Maia Compositor on WAYLAND_DISPLAY=%s", socket);
+    maia_info("export  WAYLAND_DISPLAY=%s\n", socket);
+    printf("export  WAYLAND_DISPLAY=%s\n", socket);
+
+    // RUN EVENT LOOP
+    wl_display_run(server.wl_display);
+
+    //* QUIT *//
+    wl_display_destroy(server.wl_display);
+
     return 0;
   }
-
-  struct tinywl_server server = {0};
-
-  server.wl_display = wl_display_create();
-  if (server.wl_display == NULL) {
-    wlr_log(WLR_ERROR, "Failed to create wayland display");
-    return 1;
-  }
-
-  server.backend = wlr_backend_autocreate(wl_display_get_event_loop(server.wl_display), NULL);
-  if (server.backend == NULL) {
-    wlr_log(WLR_ERROR, "failed to create wlr_backend");
-    return 1;
-  }
-
-  server.renderer = wlr_renderer_autocreate(server.backend);
-  if(server.renderer == NULL){
-    wlr_log(WLR_ERROR, "failed to create wlr_renderer");
-    return 1;
-  }
-
-  wlr_renderer_init_wl_display(server.renderer, server.wl_display);
-
-  //CREATE GBM BUFFERS
-  server.allocator = wlr_allocator_autocreate(server.backend, server.renderer);
-  if(server.allocator == NULL){
-    wlr_log(WLR_ERROR, "failed to create wlr_allocator");
-    return 1;
-  }
-
-  // INITIALIZE WAYLAND CORE GLOBALS
-
-  // CORE COMPOSITOR: HANDLES SURFACES AND BINDS GRAPHICS RENDERER
-  wlr_compositor_create(server.wl_display, 5, server.renderer);
-
-  // SUBCOMPOSITOR: ALLOWS NESTED SURFACES (E.G., BROWSER TABS/VIDEO PLAYERS)
-  wlr_subcompositor_create(server.wl_display);
-
-  // DATA DEVICE MANAGER: ENABLES CLIPBOARD AND DRAG-AND-DROP
-  wlr_data_device_manager_create(server.wl_display);
-
-  //?????????????????????????????????????????????????????????????????
-  //server.output_layout = wlr_output_layout_create(server.wl_display);
-  server.output_layout = wlr_output_layout_create(server.wl_display);
-
-  wl_list_init(&server.outputs);
-
-  // SET CALLBACK FOR NEW MONITOR
-  server.new_output.notify = server_new_output;
-  wl_signal_add(&server.backend->events.new_output, &server.new_output);
-
-  server.scene = wlr_scene_create();
-  server.scene_layout = wlr_scene_attach_output_layout(server.scene, server.output_layout);
-
-
-
-
-  //------------------ RUN -----------------------
-
-
-  //Add a Unix socket to the Wayland display
-  const char *socket = wl_display_add_socket_auto(server.wl_display);
-  if(!socket){
-    wlr_backend_destroy(server.backend);
-    return 1;
-  }
-
-  // START THE BACKEND. THIS WILL ENUMERATE OUTPUTS AND INPUTS, BECOME THE DRM MASTER, etc
-  if(!wlr_backend_start(server.backend)){
-    wlr_backend_destroy(server.backend);
-    wl_display_destroy(server.wl_display);
-  }
-
-  //SET ENVIROMENT VARIABLE
-  setenv("WAYLAND_DISPLAY", socket, true);
-  if (startup_cmd) {
-    if (fork() == 0) {
-      execl("/bin/sh", "/bin/sh", "-c", startup_cmd, (void *)NULL);
-    }
-  }
-
-  wlr_log(WLR_INFO, "Running Maia Compositor on WAYLAND_DISPLAY=%s", socket);
-
-  // RUN EVENT LOOP
-  wl_display_run(server.wl_display);
-
-  return 0;
-}
